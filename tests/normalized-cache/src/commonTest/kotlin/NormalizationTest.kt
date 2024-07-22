0ccc736ca5d5fe7e311dd1736f8e0cfc7b9faef2
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-07-22 19:49:34 +0200

    Fix normalization of variable default values (#23)
    * Fix tests included build
    
    * Fix normalization of variable default values

---
+++ tests/normalized-cache/src/commonTest/kotlin/NormalizationTest.kt
@@ -0,0 +1,36 @@
+package test
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.testing.QueueTestNetworkTransport
+import com.apollographql.apollo.testing.enqueueTestResponse
+import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.cache.normalized.FetchPolicy
+import com.apollographql.cache.normalized.api.MemoryCacheFactory
+import com.apollographql.cache.normalized.fetchPolicy
+import com.apollographql.cache.normalized.normalizedCache
+import sqlite.RepositoryListQuery
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class NormalizationTest {
+  @Test
+  fun variableDefaultValuesTest() = runTest {
+    val apolloClient = ApolloClient.Builder()
+      .networkTransport(QueueTestNetworkTransport())
+      .normalizedCache(MemoryCacheFactory())
+      .build()
+    val query = RepositoryListQuery()
+    apolloClient.enqueueTestResponse(
+      query,
+      RepositoryListQuery.Data(
+        listOf(RepositoryListQuery.Repository("42"))
+      )
+    )
+    apolloClient.query(query).execute()
+    val response = apolloClient
+      .query(query)
+      .fetchPolicy(FetchPolicy.CacheOnly)
+      .execute()
+    assertEquals("42", response.data!!.repositories.first().id)
+  }
+}
