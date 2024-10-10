e725c9d721b39d76a19cab7c61f1dcd3a8c32122
BoD <BoD@JRAF.org>
2024-10-10 11:58:50 +0200

    Provide cache dump

--- tests/settings.gradle.kts
+++ tests/settings.gradle.kts
@@ -3,6 +3,9 @@ includeBuild("../")
 pluginManagement {
   listOf(repositories, dependencyResolutionManagement.repositories).forEach {
     it.apply {
+      maven {
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+      }
       mavenCentral()
     }
   }
