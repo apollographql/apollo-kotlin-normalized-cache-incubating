850b3527bdb914dddefcbc00afb67d7b6eb9554e
BoD <BoD@JRAF.org>
2024-10-18 19:27:01 +0200

    Add a test for queryNetworkIfStale

--- tests/cache-control/src/commonTest/kotlin/ClientAndServerSideCacheControlTest.kt
+++ tests/cache-control/src/commonTest/kotlin/ClientAndServerSideCacheControlTest.kt
@@ -1,10 +1,14 @@
 package test
 
+import app.cash.turbine.test
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.ApolloResponse
+import com.apollographql.apollo.api.Query
 import com.apollographql.apollo.exception.CacheMissException
 import com.apollographql.apollo.testing.internal.runTest
 import com.apollographql.cache.normalized.FetchPolicy
 import com.apollographql.cache.normalized.api.CacheControlCacheResolver
+import com.apollographql.cache.normalized.api.GlobalMaxAgeProvider
 import com.apollographql.cache.normalized.api.MaxAge
 import com.apollographql.cache.normalized.api.NormalizedCacheFactory
 import com.apollographql.cache.normalized.api.SchemaCoordinatesMaxAgeProvider
@@ -12,6 +16,7 @@ import com.apollographql.cache.normalized.apolloStore
 import com.apollographql.cache.normalized.cacheHeaders
 import com.apollographql.cache.normalized.cacheInfo
 import com.apollographql.cache.normalized.fetchPolicy
+import com.apollographql.cache.normalized.isFromCache
 import com.apollographql.cache.normalized.maxStale
 import com.apollographql.cache.normalized.memory.MemoryCacheFactory
 import com.apollographql.cache.normalized.normalizedCache
@@ -19,11 +24,16 @@ import com.apollographql.cache.normalized.sql.SqlNormalizedCacheFactory
 import com.apollographql.cache.normalized.storeExpirationDate
 import com.apollographql.mockserver.MockResponse
 import com.apollographql.mockserver.MockServer
+import com.apollographql.mockserver.enqueueString
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import programmatic.GetUserEmailQuery
 import programmatic.GetUserNameQuery
 import programmatic.GetUserQuery
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
+import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.seconds
 
 class ClientAndServerSideCacheControlTest {
@@ -185,4 +195,113 @@ class ClientAndServerSideCacheControlTest {
     assertTrue(userEmailResponse.data?.user?.email == "john@doe.com")
     assertTrue(userEmailResponse.cacheInfo?.isStale == true)
   }
+
+  @Test
+  fun queryNetworkIfStaleMemory() {
+    queryNetworkIfStale(MemoryCacheFactory())
+  }
+
+  @Test
+  fun queryNetworkIfStaleSql() {
+    queryNetworkIfStale(SqlNormalizedCacheFactory())
+  }
+
+  @Test
+  fun queryNetworkIfStaleChained() {
+    queryNetworkIfStale(MemoryCacheFactory().chain(SqlNormalizedCacheFactory()))
+  }
+
+  private fun queryNetworkIfStale(normalizedCacheFactory: NormalizedCacheFactory) = runTest {
+    val mockServer = MockServer()
+    val client = ApolloClient.Builder()
+        .normalizedCache(
+            normalizedCacheFactory = normalizedCacheFactory,
+            cacheResolver = CacheControlCacheResolver(GlobalMaxAgeProvider(1.days)),
+        )
+        .serverUrl(mockServer.url())
+        .build()
+    client.apolloStore.clearAll()
+
+    val data = """
+      {
+        "data": {
+          "user": {
+            "name": "John",
+            "email": "john@doe.com",
+            "admin": true
+          }
+        }
+      }
+    """.trimIndent()
+
+    val query = GetUserQuery()
+
+    // Store data with a received date of now
+    mockServer.enqueueString(data)
+    client.query(GetUserQuery()).fetchPolicy(FetchPolicy.NetworkOnly)
+        .cacheHeaders(receivedDate(currentTimeSeconds()))
+        .execute()
+    // Should get 1 item from the cache (fresh)
+    client.queryNetworkIfStale(query).test {
+      val response = awaitItem()
+      assertTrue(response.data?.user?.name == "John")
+      assertTrue(response.cacheInfo?.isCacheHit == true)
+      assertTrue(response.cacheInfo?.isStale == false)
+      awaitComplete()
+    }
+
+    // Store data with a received date of 1.5 days ago
+    mockServer.enqueueString(data)
+    client.query(GetUserQuery()).fetchPolicy(FetchPolicy.NetworkOnly)
+        .cacheHeaders(receivedDate(currentTimeSeconds() - 1.5.days.inWholeSeconds))
+        .execute()
+    // Should get 2 items: 1 from the cache (stale within max stale) and 1 from the network
+    mockServer.enqueueString(data)
+    client.queryNetworkIfStale(query).test {
+      val response1 = awaitItem()
+      assertTrue(response1.data?.user?.name == "John")
+      assertTrue(response1.cacheInfo?.isCacheHit == true)
+      assertTrue(response1.cacheInfo?.isStale == true)
+      val response2 = awaitItem()
+      assertTrue(response2.data?.user?.name == "John")
+      assertFalse(response2.isFromCache)
+      awaitComplete()
+    }
+
+    // Store data with a received date of 2.5 days ago
+    mockServer.enqueueString(data)
+    client.query(GetUserQuery()).fetchPolicy(FetchPolicy.NetworkOnly)
+        .cacheHeaders(receivedDate(currentTimeSeconds() - 2.5.days.inWholeSeconds))
+        .execute()
+    // Should get 1 item from the network (stale above max stale)
+    mockServer.enqueueString(data)
+    client.queryNetworkIfStale(query).test {
+      val response = awaitItem()
+      assertTrue(response.data?.user?.name == "John")
+      assertFalse(response.isFromCache)
+      awaitComplete()
+    }
+  }
 }
+
+private fun <D : Query.Data> ApolloClient.queryNetworkIfStale(query: Query<D>): Flow<ApolloResponse<D>> = flow {
+  val cacheResponse = query(query).fetchPolicy(FetchPolicy.CacheOnly).maxStale(1.days).execute()
+  when {
+    cacheResponse.exception is CacheMissException -> {
+      // Stale above maxStale (or not cached): emit the network response only
+      emit(query(query).fetchPolicy(FetchPolicy.NetworkOnly).execute())
+    }
+
+    cacheResponse.cacheInfo?.isStale == true -> {
+      // Stale within maxStale: emit the cache response and also the network response
+      emit(cacheResponse)
+      emit(query(query).fetchPolicy(FetchPolicy.NetworkOnly).execute())
+    }
+
+    else -> {
+      // Fresh: emit the cache response only
+      emit(cacheResponse)
+    }
+  }
+}
+
