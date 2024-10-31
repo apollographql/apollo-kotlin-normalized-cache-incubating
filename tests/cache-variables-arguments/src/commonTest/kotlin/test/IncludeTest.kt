489396c5d54b8000bb3c88fd713f981585fb1add
BoD <BoD@JRAF.org>
2024-10-31 18:58:48 +0100

    Add tests/cache-variables-arguments

---
+++ tests/cache-variables-arguments/src/commonTest/kotlin/test/IncludeTest.kt
@@ -0,0 +1,35 @@
+package test
+
+import cache.include.GetUserQuery
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.cache.normalized.FetchPolicy
+import com.apollographql.cache.normalized.apolloStore
+import com.apollographql.cache.normalized.fetchPolicy
+import com.apollographql.cache.normalized.memory.MemoryCacheFactory
+import com.apollographql.cache.normalized.normalizedCache
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class IncludeTest {
+  @Test
+  fun simple() = runTest {
+    val client = ApolloClient.Builder()
+        .normalizedCache(MemoryCacheFactory())
+        .serverUrl("https://unused.com")
+        .build()
+
+    val operation = GetUserQuery(withDetails = false)
+
+    val data = GetUserQuery.Data(
+        user = GetUserQuery.User(__typename = "User", id = "42", userDetails = null)
+    )
+
+    client.apolloStore.writeOperation(operation, data)
+
+    val response = client.query(operation).fetchPolicy(FetchPolicy.CacheOnly).execute()
+
+    assertEquals("42", response.data?.user?.id)
+    assertEquals(null, response.data?.user?.userDetails)
+  }
+}
