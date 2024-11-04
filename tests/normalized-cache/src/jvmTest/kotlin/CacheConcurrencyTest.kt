6c3b99b3eca1f54ae725a58002cf43341f1dac10
BoD <BoD@JRAF.org>
2024-11-04 12:31:01 +0100

    Add tests/normalized-cache JVM tests

---
+++ tests/normalized-cache/src/jvmTest/kotlin/CacheConcurrencyTest.kt
@@ -0,0 +1,44 @@
+package test
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.testing.QueueTestNetworkTransport
+import com.apollographql.apollo.testing.enqueueTestResponse
+import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.cache.normalized.ApolloStore
+import com.apollographql.cache.normalized.memory.MemoryCacheFactory
+import com.apollographql.cache.normalized.store
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import normalizer.CharacterNameByIdQuery
+import java.util.concurrent.Executors
+import kotlin.test.Test
+
+class CacheConcurrencyTest {
+
+  @Test
+  fun storeConcurrently() = runTest {
+    val store = ApolloStore(MemoryCacheFactory(maxSizeBytes = 1000))
+    val executor = Executors.newFixedThreadPool(10)
+    val dispatcher = executor.asCoroutineDispatcher()
+
+    val apolloClient = ApolloClient.Builder()
+        .networkTransport(QueueTestNetworkTransport())
+        .store(store)
+        .dispatcher(dispatcher)
+        .build()
+
+    val concurrency = 100
+
+    0.until(concurrency).map {
+      launch(dispatcher) {
+        val query = CharacterNameByIdQuery((it / 2).toString())
+        apolloClient.enqueueTestResponse(query, CharacterNameByIdQuery.Data(CharacterNameByIdQuery.Character(name = it.toString())))
+        apolloClient.query(query).execute()
+      }
+    }.joinAll()
+
+    executor.shutdown()
+    println(store.dump().values.toList()[1].map { (k, v) -> "$k -> ${v.fields}" }.joinToString("\n"))
+  }
+}
---
+++ tests/normalized-cache/src/jvmTest/kotlin/WriteToCacheAsynchronouslyTest.kt
@@ -0,0 +1,86 @@
+package test
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.cache.normalized.ApolloStore
+import com.apollographql.cache.normalized.api.CacheHeaders
+import com.apollographql.cache.normalized.memory.MemoryCacheFactory
+import com.apollographql.cache.normalized.store
+import com.apollographql.cache.normalized.writeToCacheAsynchronously
+import com.apollographql.mockserver.MockServer
+import com.apollographql.mockserver.enqueueString
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.withContext
+import normalizer.HeroAndFriendsNamesQuery
+import normalizer.type.Episode
+import java.util.concurrent.Executors
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+/**
+ * These tests are only on the JVM as on native all the cache operations are serialized so it's impossible to read the cache before it
+ * has been written and confirm/infirm the test. Maybe we could do something with an AtomicReference or something like this
+ */
+class WriteToCacheAsynchronouslyTest {
+  private lateinit var mockServer: MockServer
+  private lateinit var apolloClient: ApolloClient
+  private lateinit var store: ApolloStore
+  private var dispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+
+  private suspend fun setUp() {
+    store = ApolloStore(MemoryCacheFactory())
+    mockServer = MockServer()
+    apolloClient = ApolloClient.Builder()
+        .serverUrl(mockServer.url())
+        .dispatcher(dispatcher)
+        .store(store)
+        .build()
+  }
+
+  private suspend fun tearDown() {
+    mockServer.close()
+    dispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+  }
+
+  /**
+   * Write to cache asynchronously, make sure records are not in cache when we receive the response
+   */
+  @Test
+  fun writeToCacheAsynchronously() = runTest({ setUp() }, { tearDown() }) {
+    withContext(dispatcher) {
+      val query = HeroAndFriendsNamesQuery(Episode.JEDI)
+
+      mockServer.enqueueString(testFixtureToUtf8("HeroAndFriendsNameResponse.json"))
+      apolloClient.query(query)
+          .writeToCacheAsynchronously(true)
+          .execute()
+
+
+      val record = store.accessCache { it.loadRecord(QUERY_ROOT_KEY, CacheHeaders.NONE) }
+      assertNull(record)
+    }
+  }
+
+  /**
+   * Write to cache synchronously, make sure records are in cache when we receive the response
+   */
+  @Test
+  fun writeToCacheSynchronously() = runTest({ setUp() }, { tearDown() }) {
+    withContext(dispatcher) {
+      val query = HeroAndFriendsNamesQuery(Episode.JEDI)
+
+      mockServer.enqueueString(testFixtureToUtf8("HeroAndFriendsNameResponse.json"))
+      apolloClient.query(query)
+          .writeToCacheAsynchronously(false)
+          .execute()
+
+      val record = store.accessCache { it.loadRecord(QUERY_ROOT_KEY, CacheHeaders.NONE) }
+      assertNotNull(record)
+    }
+  }
+
+  companion object {
+    const val QUERY_ROOT_KEY = "QUERY_ROOT"
+  }
+}
