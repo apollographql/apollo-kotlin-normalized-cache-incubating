e32c1597d65a9305b6a2ffbb785e0338420e6b17
BoD <BoD@JRAF.org>
2024-11-04 14:44:08 +0100

    Add tests/models-operation-based

---
+++ tests/models-response-based/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.apollo)
+}
+
+kotlin {
+  configureKmp(
+      withJs = true,
+      withWasm = false,
+      withAndroid = false,
+      withApple = AppleTargets.Host,
+  )
+
+  sourceSets {
+    getByName("commonMain") {
+      dependencies {
+        implementation(libs.apollo.runtime)
+        implementation("com.apollographql.cache:normalized-cache-incubating")
+      }
+    }
+
+    getByName("commonTest") {
+      dependencies {
+        implementation(libs.apollo.testing.support)
+        implementation(libs.apollo.mockserver)
+        implementation(libs.kotlin.test)
+      }
+    }
+  }
+}
+
+apollo {
+  service("fixtures") {
+    srcDir(file("../models-fixtures/graphql"))
+    packageName.set("codegen.models")
+    generateFragmentImplementations.set(true)
+    mapScalar("Date", "kotlin.Long")
+    codegenModels.set("responseBased")
+    sealedClassesForEnumsMatching.set(setOf("StarshipType"))
+  }
+}
---
+++ tests/models-response-based/src/commonTest/kotlin/test/BasicTest.kt
@@ -0,0 +1,109 @@
+package test
+
+import codegen.models.HeroHumanOrDroidQuery
+import codegen.models.HeroParentTypeDependentFieldQuery
+import codegen.models.HeroParentTypeDependentFieldQuery.Data.DroidHero.Friend.Companion.asHuman
+import codegen.models.HeroParentTypeDependentFieldQuery.Data.Hero.Companion.asDroid
+import codegen.models.MergedFieldWithSameShapeQuery
+import codegen.models.MergedFieldWithSameShapeQuery.Data.Hero.Companion.asDroid
+import codegen.models.MergedFieldWithSameShapeQuery.Data.Hero.Companion.asHuman
+import codegen.models.type.Episode
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.ApolloResponse
+import com.apollographql.apollo.api.Optional
+import com.apollographql.apollo.api.Query
+import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.cache.normalized.ApolloStore
+import com.apollographql.cache.normalized.FetchPolicy
+import com.apollographql.cache.normalized.api.IdCacheKeyGenerator
+import com.apollographql.cache.normalized.fetchPolicy
+import com.apollographql.cache.normalized.memory.MemoryCacheFactory
+import com.apollographql.cache.normalized.store
+import com.apollographql.mockserver.MockServer
+import com.apollographql.mockserver.enqueueString
+import testFixtureToUtf8
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class BasicTest {
+  private lateinit var mockServer: MockServer
+  private lateinit var apolloClient: ApolloClient
+  private lateinit var store: ApolloStore
+
+  private suspend fun setUp() {
+    store = ApolloStore(
+        normalizedCacheFactory = MemoryCacheFactory(),
+        cacheKeyGenerator = IdCacheKeyGenerator()
+    )
+    mockServer = MockServer()
+    apolloClient = ApolloClient.Builder().serverUrl(mockServer.url()).store(store).build()
+  }
+
+  private suspend fun tearDown() {
+    mockServer.close()
+  }
+
+  private fun <D : Query.Data> basicTest(resourceName: String, query: Query<D>, block: ApolloResponse<D>.() -> Unit) =
+    runTest(before = { setUp() }, after = { tearDown() }) {
+      mockServer.enqueueString(testFixtureToUtf8(resourceName))
+      var response = apolloClient.query(query).fetchPolicy(FetchPolicy.NetworkOnly).execute()
+      response.block()
+      response = apolloClient.query(query).fetchPolicy(FetchPolicy.CacheOnly).execute()
+      response.block()
+    }
+
+  @Test
+  @Throws(Exception::class)
+  fun heroParentTypeDependentField() = basicTest(
+      "HeroParentTypeDependentField.json",
+      HeroParentTypeDependentFieldQuery(Optional.Present(Episode.NEWHOPE))
+  ) {
+
+    assertFalse(hasErrors())
+    assertEquals(data?.hero?.name, "R2-D2")
+    assertEquals(data?.hero?.name, "R2-D2")
+    val hero = data?.hero?.asDroid()!!
+    assertEquals(hero.friends?.size, 3)
+    assertEquals(hero.friends?.get(0)?.name, "Luke Skywalker")
+    assertEquals(hero.friends?.get(0)?.name, "Luke Skywalker")
+    assertEquals((hero.friends?.get(0)?.asHuman())?.height, 1.72)
+  }
+
+
+  @Test
+  fun polymorphicDroidFieldsGetParsedToDroid() = basicTest(
+      "MergedFieldWithSameShape_Droid.json",
+      MergedFieldWithSameShapeQuery(Optional.Present(Episode.NEWHOPE))
+  ) {
+
+    assertFalse(hasErrors())
+    assertTrue(data?.hero is MergedFieldWithSameShapeQuery.Data.DroidHero)
+    assertEquals(data?.hero?.asDroid()?.property, "Astromech")
+  }
+
+  @Test
+  fun polymorphicHumanFieldsGetParsedToHuman() = basicTest(
+      "MergedFieldWithSameShape_Human.json",
+      MergedFieldWithSameShapeQuery(Optional.Present(Episode.NEWHOPE))
+  ) {
+
+    assertFalse(hasErrors())
+    assertTrue(data?.hero is MergedFieldWithSameShapeQuery.Data.HumanHero)
+    assertEquals(data?.hero?.asHuman()?.property, "Tatooine")
+  }
+
+  @Test
+  fun canUseExhaustiveWhen() = basicTest(
+      "HeroHumanOrDroid.json",
+      HeroHumanOrDroidQuery(Optional.Present(Episode.NEWHOPE))
+  ) {
+    val name = when (val hero = data!!.hero!!) {
+      is HeroHumanOrDroidQuery.Data.DroidHero -> hero.name
+      is HeroHumanOrDroidQuery.Data.HumanHero -> hero.name
+      is HeroHumanOrDroidQuery.Data.OtherHero -> hero.name
+    }
+    assertEquals(name, "R2-D2")
+  }
+}
