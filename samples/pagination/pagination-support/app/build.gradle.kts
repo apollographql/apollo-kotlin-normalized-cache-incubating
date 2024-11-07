9ad1b1d604d9740d9e24e92a053020f45bff703c
BoD <BoD@JRAF.org>
2024-11-07 16:37:51 +0100

    Update dependencies in samples

--- samples/pagination/pagination-support/app/build.gradle.kts
+++ samples/pagination/pagination-support/app/build.gradle.kts
@@ -3,8 +3,8 @@ fun prop(key: String) = project.findProperty(key).toString()
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.apollographql.apollo").version("4.0.2-SNAPSHOT")
-    id("org.jetbrains.kotlin.plugin.compose").version("2.0.0")
+    id("com.apollographql.apollo").version("4.1.0")
+    id("org.jetbrains.kotlin.plugin.compose").version("2.0.21")
 }
 
 android {
@@ -67,17 +67,17 @@ apollo {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
     implementation("com.apollographql.apollo:apollo-runtime")
-    implementation("com.apollographql.cache:normalized-cache-sqlite-incubating:0.0.4-SNAPSHOT")
+    implementation("com.apollographql.cache:normalized-cache-sqlite-incubating:0.0.4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
