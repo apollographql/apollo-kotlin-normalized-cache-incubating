3b21f209be5536d31818a22f113aec40e516dbd1
BoD <BoD@JRAF.org>
2025-02-17 18:59:52 +0100

    Minor ApolloJsonElementSerializer optimizations

--- normalized-cache-sqlite-incubating/src/commonMain/kotlin/com/apollographql/cache/normalized/sql/internal/ApolloJsonElementSerializer.kt
+++ normalized-cache-sqlite-incubating/src/commonMain/kotlin/com/apollographql/cache/normalized/sql/internal/ApolloJsonElementSerializer.kt
@@ -23,30 +23,66 @@ internal object ApolloJsonElementSerializer {
   }
 
   private fun Buffer.writeString(value: String) {
-    // TODO: special case for empty string, saves 4 bytes
-    writeInt(value.utf8Size().toInt()) // TODO: sizes should be unsigned
+    writeNumber(value.utf8Size())
     writeUtf8(value)
   }
 
   private fun Buffer.readString(): String {
-    return readUtf8(readInt().toLong())
+    return readUtf8(readNumber().toLong())
+  }
+
+  private fun Buffer.writeNumber(value: Number) {
+    when (value.toLong()) {
+      0L -> {
+        writeByte(NUMBER_0)
+      }
+
+      in Byte.MIN_VALUE..Byte.MAX_VALUE -> {
+        writeByte(NUMBER_BYTE)
+        writeByte(value.toInt())
+      }
+
+      in Short.MIN_VALUE..Short.MAX_VALUE -> {
+        writeByte(NUMBER_SHORT)
+        writeShort(value.toInt())
+      }
+
+      in Int.MIN_VALUE..Int.MAX_VALUE -> {
+        writeByte(NUMBER_INT)
+        writeInt(value.toInt())
+      }
+
+      else -> {
+        writeByte(NUMBER_LONG)
+        writeLong(value.toLong())
+      }
+    }
+  }
+
+  private fun Buffer.readNumber(): Number {
+    return when (val what = readByte().toInt()) {
+      NUMBER_0 -> 0
+      NUMBER_BYTE -> readByte()
+      NUMBER_SHORT -> readShort()
+      NUMBER_INT -> readInt()
+      NUMBER_LONG -> readLong()
+      else -> error("Trying to read unsupported Number type: $what")
+    }
   }
 
   private fun Buffer.writeAny(value: ApolloJsonElement) {
     when (value) {
       is String -> {
-        buffer.writeByte(STRING)
-        buffer.writeString(value)
+        if (value.isEmpty()) {
+          writeByte(EMPTY_STRING)
+        } else {
+          writeByte(STRING)
+          writeString(value)
+        }
       }
 
-      is Int -> {
-        buffer.writeByte(INT)
-        buffer.writeInt(value)
-      }
-
-      is Long -> {
-        buffer.writeByte(LONG)
-        buffer.writeLong(value)
+      is Int, is Long -> {
+        writeNumber(value)
       }
 
       is Double -> {
@@ -60,8 +96,11 @@ internal object ApolloJsonElementSerializer {
       }
 
       is Boolean -> {
-        buffer.writeByte(BOOLEAN) // TODO: 1 byte for BOOLEAN_TRUE, 1 byte for BOOLEAN_FALSE
-        buffer.writeByte(if (value) 1 else 0)
+        if (value) {
+          buffer.writeByte(BOOLEAN_TRUE)
+        } else {
+          buffer.writeByte(BOOLEAN_FALSE)
+        }
       }
 
       is CacheKey -> {
@@ -70,21 +109,29 @@ internal object ApolloJsonElementSerializer {
       }
 
       is List<*> -> {
-        buffer.writeByte(LIST) // TODO: special case for empty list, saves 4 bytes
-        buffer.writeInt(value.size) // TODO: sizes should be unsigned
-        value.forEach {
-          buffer.writeAny(it)
+        if (value.isEmpty()) {
+          buffer.writeByte(EMPTY_LIST)
+        } else {
+          buffer.writeByte(LIST)
+          buffer.writeNumber(value.size)
+          value.forEach {
+            buffer.writeAny(it)
+          }
         }
       }
 
       is Map<*, *> -> {
-        buffer.writeByte(MAP) // TODO: special case for empty map, saves 4 bytes
-        buffer.writeInt(value.size) // TODO: sizes should be unsigned
-        @Suppress("UNCHECKED_CAST")
-        value as Map<String, Any?>
-        value.forEach {
-          buffer.writeString(it.key)
-          buffer.writeAny(it.value)
+        if (value.isEmpty()) {
+          buffer.writeByte(MAP_EMPTY)
+        } else {
+          buffer.writeByte(MAP)
+          buffer.writeNumber(value.size)
+          @Suppress("UNCHECKED_CAST")
+          value as Map<String, Any?>
+          value.forEach {
+            buffer.writeString(it.key)
+            buffer.writeAny(it.value)
+          }
         }
       }
 
@@ -99,42 +146,56 @@ internal object ApolloJsonElementSerializer {
   private fun Buffer.readAny(): ApolloJsonElement {
     return when (val what = readByte().toInt()) {
       STRING -> readString()
-      INT -> readInt()
-      LONG -> readLong()
+      EMPTY_STRING -> ""
+      NUMBER_0 -> 0
+      NUMBER_BYTE -> readByte().toInt()
+      NUMBER_SHORT -> readShort().toInt()
+      NUMBER_INT -> readInt()
+      NUMBER_LONG -> readLong()
       DOUBLE -> Double.fromBits(readLong())
       JSON_NUMBER -> JsonNumber(readString())
-      BOOLEAN -> readByte() > 0
+      BOOLEAN_TRUE -> true
+      BOOLEAN_FALSE -> false
       CACHE_KEY -> {
         CacheKey(readString())
       }
 
       LIST -> {
-        val size = readInt()
+        val size = readNumber().toInt()
         0.until(size).map {
           readAny()
         }
       }
+      EMPTY_LIST -> emptyList<ApolloJsonElement>()
 
       MAP -> {
-        val size = readInt()
+        val size = readNumber().toInt()
         0.until(size).associate {
           readString() to readAny()
         }
       }
+      MAP_EMPTY -> emptyMap<String, ApolloJsonElement>()
 
       NULL -> null
-      else -> error("Trying to read unsupported Record value: $what")
+      else -> error("Trying to read unsupported Record type: $what")
     }
   }
 
   private const val STRING = 0
-  private const val INT = 1
-  private const val LONG = 2 // TODO replace INT and LONG by BYTE, UBYTE, SHORT, USHORT, UINT for smaller values
-  private const val BOOLEAN = 3
-  private const val DOUBLE = 4
-  private const val JSON_NUMBER = 5
-  private const val LIST = 6
-  private const val MAP = 7
-  private const val CACHE_KEY = 8
-  private const val NULL = 9
+  private const val EMPTY_STRING = 1
+  private const val NUMBER_0 = 2
+  private const val NUMBER_BYTE = 3
+  private const val NUMBER_SHORT = 4
+  private const val NUMBER_INT = 5
+  private const val NUMBER_LONG = 6
+  private const val BOOLEAN_TRUE = 7
+  private const val BOOLEAN_FALSE = 8
+  private const val DOUBLE = 9
+  private const val JSON_NUMBER = 10
+  private const val LIST = 11
+  private const val EMPTY_LIST = 12
+  private const val MAP = 13
+  private const val MAP_EMPTY = 14
+  private const val CACHE_KEY = 15
+  private const val NULL = 16
 }
