7c8944c6a1a2f66cf054f6d1adc64eea4c508ec6
BoD <BoD@JRAF.org>
2025-03-28 18:59:31 +0100

    Use more parameters when possible

--- normalized-cache-sqlite-incubating/src/androidMain/kotlin/com/apollographql/cache/normalized/sql/internal/factoryImplementations.android.kt
+++ normalized-cache-sqlite-incubating/src/androidMain/kotlin/com/apollographql/cache/normalized/sql/internal/factoryImplementations.android.kt
@@ -1,12 +1,12 @@
 package com.apollographql.cache.normalized.sql.internal
 
+import android.os.Build
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import app.cash.sqldelight.db.QueryResult
 import com.apollographql.cache.normalized.sql.ApolloInitializer
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
-
 
 internal actual fun createDriver(name: String?, baseDir: String?, schema: SqlSchema<QueryResult.Value<Unit>>): SqlDriver {
   check(baseDir == null) {
@@ -22,4 +22,12 @@ internal actual fun createDriver(name: String?, baseDir: String?, schema: SqlSch
 
 internal actual fun maybeCreateOrMigrateSchema(driver: SqlDriver, schema: SqlSchema<QueryResult.Value<Unit>>) {
   // no-op
+}
+
+// See https://www.sqlite.org/limits.html#:~:text=Maximum%20Number%20Of%20Host%20Parameters
+// and https://developer.android.com/reference/android/database/sqlite/package-summary.html
+internal actual val parametersMax: Int = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+  999
+} else {
+  32767
 }
