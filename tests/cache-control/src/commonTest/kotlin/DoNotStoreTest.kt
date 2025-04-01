40cd000208dbfdfc0fc6d482e3caf58621f1d117
BoD <BoD@JRAF.org>
2025-04-01 20:02:55 +0200

    Add a test showing how to avoid storing certain arguments to the cache

--- tests/cache-control/src/commonTest/kotlin/DoNotStoreTest.kt
+++ tests/cache-control/src/commonTest/kotlin/DoNotStoreTest.kt
@@ -9,7 +9,11 @@ import com.apollographql.apollo.interceptor.ApolloInterceptor
 import com.apollographql.apollo.interceptor.ApolloInterceptorChain
 import com.apollographql.apollo.testing.internal.runTest
 import com.apollographql.cache.normalized.FetchPolicy
+import com.apollographql.cache.normalized.api.CacheHeaders
 import com.apollographql.cache.normalized.api.CacheKey
+import com.apollographql.cache.normalized.api.DefaultFieldKeyGenerator
+import com.apollographql.cache.normalized.api.FieldKeyContext
+import com.apollographql.cache.normalized.api.FieldKeyGenerator
 import com.apollographql.cache.normalized.api.NormalizedCacheFactory
 import com.apollographql.cache.normalized.api.SchemaCoordinatesMaxAgeProvider
 import com.apollographql.cache.normalized.apolloStore
@@ -19,15 +23,18 @@ import com.apollographql.cache.normalized.fetchPolicyInterceptor
 import com.apollographql.cache.normalized.memory.MemoryCacheFactory
 import com.apollographql.cache.normalized.normalizedCache
 import com.apollographql.cache.normalized.sql.SqlNormalizedCacheFactory
+import com.apollographql.cache.normalized.testing.append
 import com.apollographql.cache.normalized.testing.assertErrorsEquals
 import com.apollographql.cache.normalized.testing.keyToString
 import com.apollographql.mockserver.MockServer
 import com.apollographql.mockserver.enqueueString
 import donotstore.GetUserQuery
+import donotstore.SignInMutation
 import donotstore.cache.Cache
 import kotlinx.coroutines.flow.Flow
 import okio.use
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.time.Duration.Companion.seconds
 
@@ -43,21 +50,21 @@ class DoNotStoreTest {
   }
 
   @Test
-  fun doNotStoreMemory() {
-    doNotStore(MemoryCacheFactory())
+  fun doNotStoreQueryMemory() {
+    doNotStoreQuery(MemoryCacheFactory())
   }
 
   @Test
-  fun doNotStoreSql() {
-    doNotStore(SqlNormalizedCacheFactory())
+  fun doNotStoreQuerySql() {
+    doNotStoreQuery(SqlNormalizedCacheFactory())
   }
 
   @Test
-  fun doNotStoreChained() {
-    doNotStore(MemoryCacheFactory().chain(SqlNormalizedCacheFactory()))
+  fun doNotStoreQueryChained() {
+    doNotStoreQuery(MemoryCacheFactory().chain(SqlNormalizedCacheFactory()))
   }
 
-  private fun doNotStore(normalizedCacheFactory: NormalizedCacheFactory) = runTest(before = { setUp() }, after = { tearDown() }) {
+  private fun doNotStoreQuery(normalizedCacheFactory: NormalizedCacheFactory) = runTest(before = { setUp() }, after = { tearDown() }) {
     mockServer.enqueueString(
         // language=JSON
         """
@@ -126,6 +133,103 @@ class DoNotStoreTest {
               ),
               cacheResponse.errors
           )
+        }
+  }
+
+  @Test
+  fun doNotStoreMutationMemory() {
+    doNotStoreMutation(MemoryCacheFactory())
+  }
+
+  @Test
+  fun doNotStoreMutationSql() {
+    doNotStoreMutation(SqlNormalizedCacheFactory())
+  }
+
+  @Test
+  fun doNotStoreMutationChained() {
+    doNotStoreMutation(MemoryCacheFactory().chain(SqlNormalizedCacheFactory()))
+  }
+
+  private fun doNotStoreMutation(normalizedCacheFactory: NormalizedCacheFactory) = runTest(before = { setUp() }, after = { tearDown() }) {
+    mockServer.enqueueString(
+        // language=JSON
+        """
+        {
+          "data": {
+            "auth": {
+              "signIn": {
+                "token": "aaaabbbbccc",
+                "userData": {
+                  "__typename": "User",
+                  "id": "42",
+                  "name": "John",
+                  "email": "john@example.com"
+                }
+              }
+            }
+          }
+        }
+        """
+    )
+    val maxAgeProvider = SchemaCoordinatesMaxAgeProvider(
+        Cache.maxAges,
+        defaultMaxAge = 20.seconds,
+    )
+
+    // Do not store passwords in the cache
+    val fieldKeyGenerator = object : FieldKeyGenerator {
+      override fun getFieldKey(context: FieldKeyContext): String {
+        if (context.parentType == "Auth" && context.field.name == "signIn") {
+          return "signIn"
+        }
+        return DefaultFieldKeyGenerator.getFieldKey(context)
+      }
+    }
+    ApolloClient.Builder()
+        .serverUrl(mockServer.url())
+        .normalizedCache(
+            normalizedCacheFactory = normalizedCacheFactory,
+            maxAgeProvider = maxAgeProvider,
+            fieldKeyGenerator = fieldKeyGenerator
+        )
+        .build()
+        .use { apolloClient ->
+          apolloClient.apolloStore.clearAll()
+          val networkResponse = apolloClient.mutation(SignInMutation("scott", "tiger"))
+              .fetchPolicy(FetchPolicy.NetworkOnly)
+              .execute()
+          assertEquals(
+              SignInMutation.Data(
+                  SignInMutation.Auth(
+                      SignInMutation.SignIn(
+                          token = "aaaabbbbccc",
+                          userData = SignInMutation.UserData(
+                              __typename = "User",
+                              id = "42",
+                              userData = donotstore.fragment.UserData(
+                                  id = "42",
+                                  name = "John",
+                                  email = "john@example.com",
+                                  __typename = "User"
+
+                              )
+                          )
+                      )
+                  )
+              ),
+              networkResponse.data
+          )
+
+          apolloClient.apolloStore.accessCache { cache ->
+            val authRecord = cache.loadRecord(CacheKey("auth"), CacheHeaders.NONE)!!
+            // No password in field key
+            assertContentEquals(listOf("signIn"), authRecord.fields.keys)
+
+            val signInRecord = cache.loadRecord(CacheKey("auth").append("signIn"), CacheHeaders.NONE)!!
+            // No token in record
+            assertContentEquals(listOf("userData"), signInRecord.fields.keys)
+          }
         }
   }
 }
